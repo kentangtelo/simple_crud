e35ac209a2eefaa9e1228cffe6229e98ef223258
Iman Abdurrahman <72773420+kentangtelo@users.noreply.github.com>
2023-03-08 19:32:13 +0700

    fix: routes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var mysql = require("mysql");
 var connection = require("./lib/db");
 
 var app = express();
-
+booksRouter = require("./routes/index");
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
